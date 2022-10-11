799ee93d40b64ac29123ad593ac80b53c976579e
Erik <95243047+MegaErik228pro@users.noreply.github.com>
2022-10-12 01:32:18 +0300

    Add files via upload

---
+++ sim-slider.js
@@ -0,0 +1,109 @@
+function Sim(sldrId) {
+
+	let id = document.getElementById(sldrId);
+	if(id) {
+		this.sldrRoot = id
+	}
+	else {
+		this.sldrRoot = document.querySelector('.sim-slider')
+	};
+
+	// Slider objects
+	this.sldrList = this.sldrRoot.querySelector('.sim-slider-list');
+	this.sldrElements = this.sldrList.querySelectorAll('.sim-slider-element');
+	this.sldrElemFirst = this.sldrList.querySelector('.sim-slider-element');
+	this.indicatorDots = this.sldrRoot.querySelector('div.sim-slider-dots');
+
+	// Initialization
+	this.options = Sim.defaults;
+	Sim.initialize(this)
+};
+Sim.defaults = {
+
+	// Default options for the slider
+	dots: true      // Индикаторные точки
+};
+
+Sim.prototype.elemPrev = function(num) {
+	num = num || 1;
+
+	let prevElement = this.currentElement;
+	this.currentElement -= num;
+	if(this.currentElement < 0) this.currentElement = this.elemCount-1;
+	
+	this.sldrElements[this.currentElement].style.opacity = '1';
+	this.sldrElements[prevElement].style.opacity = '0';
+
+	if(this.options.dots) {
+		this.dotOn(prevElement); this.dotOff(this.currentElement)
+	}
+};
+
+Sim.prototype.elemNext = function(num) {
+	num = num || 1;
+	
+	let prevElement = this.currentElement;
+	this.currentElement += num;
+	if(this.currentElement >= this.elemCount) this.currentElement = 0;
+
+	this.sldrElements[this.currentElement].style.opacity = '1';
+	this.sldrElements[prevElement].style.opacity = '0';
+
+	if(this.options.dots) {
+		this.dotOn(prevElement); this.dotOff(this.currentElement)
+	}
+};
+
+Sim.prototype.dotOn = function(num) {
+	this.indicatorDotsAll[num].style.cssText =
+             'background-color:#BBB; cursor:pointer;'
+};
+
+Sim.prototype.dotOff = function(num) {
+	this.indicatorDotsAll[num].style.cssText =
+             'background-color:#556; cursor:default;'
+};
+
+Sim.initialize = function(that) {
+
+	// Constants
+	that.elemCount = that.sldrElements.length; // Количество элементов
+
+	// Variables
+	that.currentElement = 0;
+
+	// Start initialization
+	if(that.elemCount <= 1) {   // Отключить навигацию
+        that.options.dots = false
+	};
+	if(that.elemCount >= 1) {   // показать первый элемент
+		that.sldrElemFirst.style.opacity = '1';
+	};
+
+	if(that.options.dots) {  // инициализация индикаторных точек
+		let sum = '', diffNum;
+		for(let i=0; i<that.elemCount; i++) {
+			sum += '<span class="sim-dot"></span>'
+		};
+		that.indicatorDots.innerHTML = sum;
+		that.indicatorDotsAll =
+                       that.sldrRoot.querySelectorAll('span.sim-dot');
+		// Назначаем точкам обработчик события 'click'
+		for(let n=0; n<that.elemCount; n++) {
+			that.indicatorDotsAll[n].addEventListener('click', function(){
+				diffNum = Math.abs(n - that.currentElement);
+				if(n < that.currentElement) {
+					that.elemPrev(diffNum)
+				}
+				else if(n > that.currentElement) {
+					that.elemNext(diffNum)
+				}
+				// Если n == that.currentElement ничего не делаем
+			})
+		};
+		that.dotOff(0);  // точка[0] выключена, остальные включены
+		for(let i=1; i<that.elemCount; i++) {
+			that.dotOn(i)
+		}
+	}
+};
